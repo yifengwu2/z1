da849ae9d55e50df8576f2155087aae4f650484f
yifengwu2 <2454495978@qq.com>
2025-12-24 11:08:50 +0800

    练习

--- src/main/java/com/k6/Solution01.java
+++ src/main/java/com/k6/Solution01.java
@@ -1,4 +1,30 @@
 package com.k6;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+
 public class Solution01 {
+    public int maxDepth(String s) {
+        int n = s.length();
+        Deque<Character> stack = new ArrayDeque<>();
+        int cnt = 0;
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            stack.push(c);
+            if (c == '(') {
+                cnt++;
+
+            }
+            if (!stack.isEmpty() && c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    stack.pop();
+                }
+                max = Math.max(max, cnt);
+                cnt=0;
+            }
+        }
+        return max;
+    }
 }
--- src/main/java/com/k6/Solution02.java
+++ src/main/java/com/k6/Solution02.java
@@ -1,4 +1,27 @@
 package com.k6;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Solution02 {
+    public String removeOuterParentheses(String s) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        Deque<Character> stack = new ArrayDeque<>();
+        int cnt = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (cnt > 0) {
+                    sb.append(c);
+                }
+                cnt++;
+            } else {
+                cnt--;
+                if (cnt > 0) {
+                    sb.append(c);
+                }
+            }
+        }
+        return sb.toString();
+    }
 }
