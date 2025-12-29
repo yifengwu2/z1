b911915bf126f59ca61610dfa1ea664d3ed0d5c7
yifengwu2 <2454495978@qq.com>
2025-12-29 22:04:09 +0800

    观察者

--- src/main/java/com/s2/Subject.java
+++ src/main/java/com/s2/Subject.java
@@ -1,4 +1,53 @@
 package com.s2;
 
-public class Subject {
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * 被观察者
+ */
+class Subject {
+    //维护当前开关状态
+    private final AtomicBoolean enabled;
+    private final CopyOnWriteArrayList<Watch> watches;
+
+    public Subject() {
+        this.enabled = new AtomicBoolean(false);
+        this.watches = new CopyOnWriteArrayList<>();
+    }
+
+    public void enable() {
+        if (enabled.compareAndSet(false, true)) {
+            notifyWatchers(true);
+        }
+    }
+
+    public void disable() {
+        if (enabled.compareAndSet(true, false)) {
+            notifyWatchers(false);
+        }
+    }
+
+    public void notifyWatchers(boolean enable) {
+        for (Watch watch : watches) {
+            watch.onPolicyChange(enable);
+        }
+    }
+
+    public void add(Watch watch) {
+        watches.add(watch);
+    }
+
+    public void remove(Watch watch) {
+        watches.remove(watch);
+    }
+
+    public boolean isEnabled() {
+        return enabled.get();
+    }
 }
+
+interface Watch {
+    void onPolicyChange(boolean enabled);
+}
+
